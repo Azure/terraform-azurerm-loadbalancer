b40b8fccc4be9977f4da39eaf1bfe2285e065280
Yuping Wei <56525716+yupwei68@users.noreply.github.com>
2020-07-21 10:53:11 +0800

    terraform upgrade to v0.12 (#23)

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "random_id" "rg_name" {
   byte_length = 8
 }
@@ -8,17 +12,11 @@ module "mylb" {
   location            = "${var.location}"
   prefix              = "${random_id.rg_name.hex}"
 
-  "remote_port" {
+  remote_port = {
     ssh = ["Tcp", "22"]
   }
 
-  "lb_port" {
+  lb_port = {
     http = ["80", "Tcp", "80"]
   }
-}
-
-module "network" {
-  source              = "Azure/network/azurerm"
-  location            = "${var.location}"
-  resource_group_name = "${random_id.rg_name.hex}"
 }
