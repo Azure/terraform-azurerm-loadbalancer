1f1ab00e1ff844f4c4d45d49a0fb530afff1c7a0
yupwei68 <yupwei@microsoft.com>
2020-08-03 15:31:58 +0800

    update

--- outputs.tf
+++ outputs.tf
@@ -1,44 +1,44 @@
 output "azurerm_resource_group_tags" {
   description = "the tags provided for the resource group"
-  value       = "${azurerm_resource_group.azlb.tags}"
+  value       = data.azurerm_resource_group.azlb.tags
 }
 
 output "azurerm_resource_group_name" {
   description = "name of the resource group provisioned"
-  value       = "${azurerm_resource_group.azlb.name}"
+  value       = data.azurerm_resource_group.azlb.name
 }
 
 output "azurerm_lb_id" {
   description = "the id for the azurerm_lb resource"
-  value       = "${azurerm_lb.azlb.id}"
+  value       = azurerm_lb.azlb.id
 }
 
 output "azurerm_lb_frontend_ip_configuration" {
   description = "the frontend_ip_configuration for the azurerm_lb resource"
-  value       = "${azurerm_lb.azlb.frontend_ip_configuration}"
+  value       = azurerm_lb.azlb.frontend_ip_configuration
 }
 
 output "azurerm_lb_probe_ids" {
   description = "the ids for the azurerm_lb_probe resources"
-  value       = "${azurerm_lb_probe.azlb.*.id}"
+  value       = azurerm_lb_probe.azlb.*.id
 }
 
 output "azurerm_lb_nat_rule_ids" {
   description = "the ids for the azurerm_lb_nat_rule resources"
-  value       = "${azurerm_lb_nat_rule.azlb.*.id}"
+  value       = azurerm_lb_nat_rule.azlb.*.id
 }
 
 output "azurerm_public_ip_id" {
   description = "the id for the azurerm_lb_public_ip resource"
-  value       = "${azurerm_public_ip.azlb.*.id}"
+  value       = azurerm_public_ip.azlb.*.id
 }
 
 output "azurerm_public_ip_address" {
   description = "the ip address for the azurerm_lb_public_ip resource"
-  value       = "${azurerm_public_ip.azlb.*.ip_address}"
+  value       = azurerm_public_ip.azlb.*.ip_address
 }
 
 output "azurerm_lb_backend_address_pool_id" {
   description = "the id for the azurerm_lb_backend_address_pool resource"
-  value       = "${azurerm_lb_backend_address_pool.azlb.id}"
+  value       = azurerm_lb_backend_address_pool.azlb.id
 }
--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -6,17 +6,46 @@ resource "random_id" "rg_name" {
   byte_length = 8
 }
 
+resource "azurerm_resource_group" "test" {
+  name     = "example-lb-${random_id.rg_name.hex}"
+  location = "West Europe"
+}
+
 module "mylb" {
-  source              = "../../"
-  resource_group_name = "${random_id.rg_name.hex}"
-  location            = "${var.location}"
-  prefix              = "${random_id.rg_name.hex}"
+  source                                 = "../.."
+  resource_group_name                    = azurerm_resource_group.test.name
+  type                                   = "private"
+  frontend_subnet_id                     = module.network.vnet_subnets[0]
+  frontend_private_ip_address_allocation = "Static"
+  frontend_private_ip_address            = "10.0.1.6"
+  lb_sku                                 = "Standard"
 
   remote_port = {
     ssh = ["Tcp", "22"]
   }
 
   lb_port = {
-    http = ["80", "Tcp", "80"]
+    http   = ["80", "Tcp", "80"]
+    https  = ["443", "Tcp", "443"]
+    http-2 = ["80", "Tcp", "443", "/", "Https"]
+  }
+
+  tags = {
+    cost-center = "12345"
+    source      = "terraform"
+  }
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
   }
 }
