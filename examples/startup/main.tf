85a9db2526876f6511e5cbe1b24193a730b92d78
zjhe <hezijie@microsoft.com>
2023-01-09 11:00:31 +0800

    fix incorrect example

--- examples/startup/main.tf
+++ examples/startup/main.tf
@@ -43,13 +43,13 @@ module "mylb" {
 }
 
 module "network" {
-  source              = "Azure/network/azurerm"
-  version             = "4.2.0"
-  resource_group_name = azurerm_resource_group.test.name
-  address_space       = "10.0.0.0/16"
-  location            = var.location
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  source                  = "Azure/network/azurerm"
+  version                 = "4.2.0"
+  resource_group_name     = azurerm_resource_group.test.name
+  address_space           = "10.0.0.0/16"
+  resource_group_location = var.location
+  subnet_prefixes         = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names            = ["subnet1", "subnet2", "subnet3"]
 
   tags = {
     environment = "dev"
