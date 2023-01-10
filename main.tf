3596a7f6b0314a5397f26718863c6295c7d815f5
zjhe <hezijie@microsoft.com>
2023-01-10 09:11:44 +0800

    reorder code to pass tflint policy check

--- main.tf
+++ main.tf
@@ -9,68 +9,72 @@ locals {
 }
 
 resource "azurerm_public_ip" "azlb" {
-  count               = var.type == "public" ? 1 : 0
+  count = var.type == "public" ? 1 : 0
+
+  allocation_method   = var.allocation_method
+  location            = coalesce(var.location, data.azurerm_resource_group.azlb.location)
   name                = local.pip_name
   resource_group_name = data.azurerm_resource_group.azlb.name
-  location            = coalesce(var.location, data.azurerm_resource_group.azlb.location)
-  allocation_method   = var.allocation_method
   sku                 = var.pip_sku
   tags                = var.tags
 }
 
 resource "azurerm_lb" "azlb" {
+  location            = coalesce(var.location, data.azurerm_resource_group.azlb.location)
   name                = local.lb_name
   resource_group_name = data.azurerm_resource_group.azlb.name
-  location            = coalesce(var.location, data.azurerm_resource_group.azlb.location)
   sku                 = var.lb_sku
   tags                = var.tags
 
   frontend_ip_configuration {
     name                          = var.frontend_name
-    public_ip_address_id          = var.type == "public" ? join("", azurerm_public_ip.azlb.*.id) : ""
-    subnet_id                     = var.frontend_subnet_id
     private_ip_address            = var.frontend_private_ip_address
     private_ip_address_allocation = var.frontend_private_ip_address_allocation
+    public_ip_address_id          = var.type == "public" ? join("", azurerm_public_ip.azlb.*.id) : ""
+    subnet_id                     = var.frontend_subnet_id
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "azlb" {
-  name            = "BackEndAddressPool"
   loadbalancer_id = azurerm_lb.azlb.id
+  name            = "BackEndAddressPool"
 }
 
 resource "azurerm_lb_nat_rule" "azlb" {
-  count                          = length(var.remote_port)
-  name                           = "VM-${count.index}"
-  resource_group_name            = data.azurerm_resource_group.azlb.name
-  loadbalancer_id                = azurerm_lb.azlb.id
-  protocol                       = "Tcp"
-  frontend_port                  = "5000${count.index + 1}"
+  count = length(var.remote_port)
+
   backend_port                   = element(var.remote_port[element(keys(var.remote_port), count.index)], 1)
   frontend_ip_configuration_name = var.frontend_name
+  loadbalancer_id                = azurerm_lb.azlb.id
+  name                           = "VM-${count.index}"
+  protocol                       = "Tcp"
+  resource_group_name            = data.azurerm_resource_group.azlb.name
+  frontend_port                  = "5000${count.index + 1}"
 }
 
 resource "azurerm_lb_probe" "azlb" {
-  count               = length(var.lb_probe)
-  name                = element(keys(var.lb_probe), count.index)
+  count = length(var.lb_probe)
+
   loadbalancer_id     = azurerm_lb.azlb.id
-  protocol            = element(var.lb_probe[element(keys(var.lb_probe), count.index)], 0)
+  name                = element(keys(var.lb_probe), count.index)
   port                = element(var.lb_probe[element(keys(var.lb_probe), count.index)], 1)
   interval_in_seconds = var.lb_probe_interval
   number_of_probes    = var.lb_probe_unhealthy_threshold
+  protocol            = element(var.lb_probe[element(keys(var.lb_probe), count.index)], 0)
   request_path        = element(var.lb_probe[element(keys(var.lb_probe), count.index)], 2)
 }
 
 resource "azurerm_lb_rule" "azlb" {
-  count                          = length(var.lb_port)
-  name                           = element(keys(var.lb_port), count.index)
-  loadbalancer_id                = azurerm_lb.azlb.id
-  protocol                       = element(var.lb_port[element(keys(var.lb_port), count.index)], 1)
-  frontend_port                  = element(var.lb_port[element(keys(var.lb_port), count.index)], 0)
+  count = length(var.lb_port)
+
   backend_port                   = element(var.lb_port[element(keys(var.lb_port), count.index)], 2)
   frontend_ip_configuration_name = var.frontend_name
-  enable_floating_ip             = false
+  frontend_port                  = element(var.lb_port[element(keys(var.lb_port), count.index)], 0)
+  loadbalancer_id                = azurerm_lb.azlb.id
+  name                           = element(keys(var.lb_port), count.index)
+  protocol                       = element(var.lb_port[element(keys(var.lb_port), count.index)], 1)
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.azlb.id]
+  enable_floating_ip             = false
   idle_timeout_in_minutes        = 5
   probe_id                       = element(azurerm_lb_probe.azlb.*.id, count.index)
 }
--- variables.tf
+++ variables.tf
@@ -1,48 +1,6 @@
-variable "location" {
-  description = "(Optional) The location/region where the core network will be created. The full list of Azure regions can be found at https://azure.microsoft.com/regions"
-  type        = string
-  default     = ""
-}
-
 variable "resource_group_name" {
   description = "(Required) The name of the resource group where the load balancer resources will be imported."
   type        = string
-}
-
-variable "prefix" {
-  description = "(Required) Default prefix to use with your resource names."
-  type        = string
-  default     = "azure_lb"
-}
-
-variable "remote_port" {
-  description = "Protocols to be used for remote vm access. [protocol, backend_port].  Frontend port will be automatically generated starting at 50000 and in the output."
-  type        = map(any)
-  default     = {}
-}
-
-variable "lb_port" {
-  description = "Protocols to be used for lb rules. Format as [frontend_port, protocol, backend_port]"
-  type        = map(any)
-  default     = {}
-}
-
-variable "lb_probe_unhealthy_threshold" {
-  description = "Number of times the load balancer health probe has an unsuccessful attempt before considering the endpoint unhealthy."
-  type        = number
-  default     = 2
-}
-
-variable "lb_probe_interval" {
-  description = "Interval in seconds the load balancer health probe rule does a check"
-  type        = number
-  default     = 5
-}
-
-variable "frontend_name" {
-  description = "(Required) Specifies the name of the frontend ip configuration."
-  type        = string
-  default     = "myPublicIP"
 }
 
 variable "allocation_method" {
@@ -51,24 +9,10 @@ variable "allocation_method" {
   default     = "Static"
 }
 
-variable "tags" {
-  type        = map(string)
-  description = "(Optional) A mapping of tags to assign to the resource."
-  default = {
-    source = "terraform"
-  }
-}
-
-variable "type" {
-  description = "(Optional) Defined if the loadbalancer is private or public"
+variable "frontend_name" {
+  description = "(Required) Specifies the name of the frontend ip configuration."
   type        = string
-  default     = "public"
-}
-
-variable "frontend_subnet_id" {
-  description = "(Optional) Frontend subnet id to use when in private mode"
-  type        = string
-  default     = ""
+  default     = "myPublicIP"
 }
 
 variable "frontend_private_ip_address" {
@@ -83,10 +27,16 @@ variable "frontend_private_ip_address_allocation" {
   default     = "Dynamic"
 }
 
-variable "lb_sku" {
-  description = "(Optional) The SKU of the Azure Load Balancer. Accepted values are Basic and Standard."
+variable "frontend_subnet_id" {
+  description = "(Optional) Frontend subnet id to use when in private mode"
   type        = string
-  default     = "Basic"
+  default     = ""
+}
+
+variable "lb_port" {
+  description = "Protocols to be used for lb rules. Format as [frontend_port, protocol, backend_port]"
+  type        = map(any)
+  default     = {}
 }
 
 variable "lb_probe" {
@@ -95,10 +45,28 @@ variable "lb_probe" {
   default     = {}
 }
 
-variable "pip_sku" {
-  description = "(Optional) The SKU of the Azure Public IP. Accepted values are Basic and Standard."
+variable "lb_probe_interval" {
+  description = "Interval in seconds the load balancer health probe rule does a check"
+  type        = number
+  default     = 5
+}
+
+variable "lb_probe_unhealthy_threshold" {
+  description = "Number of times the load balancer health probe has an unsuccessful attempt before considering the endpoint unhealthy."
+  type        = number
+  default     = 2
+}
+
+variable "lb_sku" {
+  description = "(Optional) The SKU of the Azure Load Balancer. Accepted values are Basic and Standard."
   type        = string
   default     = "Basic"
+}
+
+variable "location" {
+  description = "(Optional) The location/region where the core network will be created. The full list of Azure regions can be found at https://azure.microsoft.com/regions"
+  type        = string
+  default     = ""
 }
 
 variable "name" {
@@ -111,4 +79,36 @@ variable "pip_name" {
   description = "(Optional) Name of public ip. If it is set, the 'prefix' variable will be ignored."
   type        = string
   default     = ""
+}
+
+variable "pip_sku" {
+  description = "(Optional) The SKU of the Azure Public IP. Accepted values are Basic and Standard."
+  type        = string
+  default     = "Basic"
+}
+
+variable "prefix" {
+  description = "(Required) Default prefix to use with your resource names."
+  type        = string
+  default     = "azure_lb"
+}
+
+variable "remote_port" {
+  description = "Protocols to be used for remote vm access. [protocol, backend_port].  Frontend port will be automatically generated starting at 50000 and in the output."
+  type        = map(any)
+  default     = {}
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "(Optional) A mapping of tags to assign to the resource."
+  default = {
+    source = "terraform"
+  }
+}
+
+variable "type" {
+  description = "(Optional) Defined if the loadbalancer is private or public"
+  type        = string
+  default     = "public"
 }
