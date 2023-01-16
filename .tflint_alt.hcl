31cb15a5b4ac24a594d0c800568a33f0024ce8af
lonegunmanb <lonegunmanb@hotmail.com>
2023-01-16 09:48:34 +0800

    reorder code to pass tflint policy check (#55)
    Co-authored-by: zjhe <hezijie@microsoft.com>

--- .tflint_alt.hcl
+++ .tflint_alt.hcl
@@ -193,11 +193,11 @@ rule "terraform_workspace_remote" {
 }
 
 rule "terraform_locals_order" {
-  enabled = false
+  enabled = true
 }
 
 rule "terraform_output_order" {
-  enabled = false
+  enabled = true
 }
 
 rule "terraform_output_separate" {
@@ -209,7 +209,7 @@ rule "terraform_variable_nullable_false" {
 }
 
 rule "terraform_variable_order" {
-  enabled = false
+  enabled = true
 }
 
 rule "terraform_variable_separate" {
@@ -217,11 +217,11 @@ rule "terraform_variable_separate" {
 }
 
 rule "terraform_resource_data_arg_layout" {
-  enabled = false
+  enabled = true
 }
 
 rule "azurerm_arg_order" {
-  enabled = false
+  enabled = true
 }
 
 rule "azurerm_resource_tag" {
--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,6 @@
-output "azurerm_resource_group_tags" {
-  description = "the tags provided for the resource group"
-  value       = data.azurerm_resource_group.azlb.tags
-}
-
-output "azurerm_resource_group_name" {
-  description = "name of the resource group provisioned"
-  value       = data.azurerm_resource_group.azlb.name
-}
-
-output "azurerm_lb_id" {
-  description = "the id for the azurerm_lb resource"
-  value       = azurerm_lb.azlb.id
+output "azurerm_lb_backend_address_pool_id" {
+  description = "the id for the azurerm_lb_backend_address_pool resource"
+  value       = azurerm_lb_backend_address_pool.azlb.id
 }
 
 output "azurerm_lb_frontend_ip_configuration" {
@@ -18,9 +8,9 @@ output "azurerm_lb_frontend_ip_configuration" {
   value       = azurerm_lb.azlb.frontend_ip_configuration
 }
 
-output "azurerm_lb_probe_ids" {
-  description = "the ids for the azurerm_lb_probe resources"
-  value       = azurerm_lb_probe.azlb.*.id
+output "azurerm_lb_id" {
+  description = "the id for the azurerm_lb resource"
+  value       = azurerm_lb.azlb.id
 }
 
 output "azurerm_lb_nat_rule_ids" {
@@ -28,9 +18,9 @@ output "azurerm_lb_nat_rule_ids" {
   value       = azurerm_lb_nat_rule.azlb.*.id
 }
 
-output "azurerm_public_ip_id" {
-  description = "the id for the azurerm_lb_public_ip resource"
-  value       = azurerm_public_ip.azlb.*.id
+output "azurerm_lb_probe_ids" {
+  description = "the ids for the azurerm_lb_probe resources"
+  value       = azurerm_lb_probe.azlb.*.id
 }
 
 output "azurerm_public_ip_address" {
@@ -38,7 +28,17 @@ output "azurerm_public_ip_address" {
   value       = azurerm_public_ip.azlb.*.ip_address
 }
 
-output "azurerm_lb_backend_address_pool_id" {
-  description = "the id for the azurerm_lb_backend_address_pool resource"
-  value       = azurerm_lb_backend_address_pool.azlb.id
+output "azurerm_public_ip_id" {
+  description = "the id for the azurerm_lb_public_ip resource"
+  value       = azurerm_public_ip.azlb.*.id
+}
+
+output "azurerm_resource_group_name" {
+  description = "name of the resource group provisioned"
+  value       = data.azurerm_resource_group.azlb.name
+}
+
+output "azurerm_resource_group_tags" {
+  description = "the tags provided for the resource group"
+  value       = data.azurerm_resource_group.azlb.tags
 }
